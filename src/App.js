d12178095d3aeed9d3879344dcc2f74f21eb6f56
rajansinghbaliyan <rajansinghbaliyan@gmail.com>
2022-02-25 15:45:46 +0530

    feat:added the test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                    <h1>
-                        Hi there its form the docker, its working now
-                    </h1>
-                </p>
+
+                <h1>
+                    Hi there its form the docker, its working now
+                </h1>
+                <h2>
+                    its definetly working i think
+                </h2>
+                <h3>
+                    Now we are inside the docker compose
+                </h3>
+
             </header>
         </div>
     );
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import React from "react";
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hi there its form the docker, its working now/);
+  expect(linkElement).toBeInTheDOM;
+});
